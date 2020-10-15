17998976d602a62c604766fd383ecd28173242df
Jinhee Kim <jinhee.kim@solarconnect.kr>
2020-10-15 19:57:30 +0900

    영화 상세 API 추가

--- graphql/db.js
+++ graphql/db.js
@@ -1,14 +1,24 @@
 import axios from 'axios';
 
 const BASE_URL = 'https://yts.am/api/v2/';
-const LIST_MOVIES_URL = `${BASE_URL}/list_movies.json`;
+const MOVIES_LIST_URL = `${BASE_URL}/list_movies.json`;
+const MOVIES_DETAIL_URL = `${BASE_URL}/movie_details.json`;
 
 export const getMovies = async (limit, rating) => {
-  const { data: { data: { movies } } } = await axios(LIST_MOVIES_URL, {
+  const { data: { data: { movies } } } = await axios(MOVIES_LIST_URL, {
     params: {
       limit,
       minimum_rating: rating
     }
   });
   return movies;
+}
+
+export const getMovie = async (id) => {
+  const { data: { data: { movie } } } = await axios(MOVIES_DETAIL_URL, {
+    params: {
+      movie_id: id,
+    }
+  });
+  return movie;
 }
