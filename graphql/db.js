36c26d0a4b002a972168f05772535200f1d1ac68
Jinhee Kim <jinhee.kim@solarconnect.kr>
2020-10-15 20:02:34 +0900

    유사한 영화 추천 API 추가

--- graphql/db.js
+++ graphql/db.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 const BASE_URL = 'https://yts.am/api/v2/';
 const MOVIES_LIST_URL = `${BASE_URL}/list_movies.json`;
 const MOVIES_DETAIL_URL = `${BASE_URL}/movie_details.json`;
+const MOVIES_SUGGESTIONS_URL = `${BASE_URL}/movie_suggestions.json`;
 
 export const getMovies = async (limit, rating) => {
   const { data: { data: { movies } } } = await axios(MOVIES_LIST_URL, {
@@ -21,4 +22,13 @@ export const getMovie = async (id) => {
     }
   });
   return movie;
+}
+
+export const getSuggestions = async (id) => {
+  const { data: { data: { movies } } } = await axios(MOVIES_SUGGESTIONS_URL, {
+    params: {
+      movie_id: id,
+    }
+  });
+  return movies;
 }
